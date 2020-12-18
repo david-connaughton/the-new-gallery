03644fac5edb300fe306acb8c76078d97fbeb546
david-connaughton <david.a.connaughton@gmail.com>
2020-12-18 19:45:19 +0000

    Pagination functionality added & Media Queries Updated

--- photos/views.py
+++ photos/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.db.models import Q
+from django.core.paginator import Paginator
 from .models import Photo
 
 
 def photos(request):
-    """A custom view to render Photo Page"""
+    """A custom view to render Photo Page with Filter & Pagination"""
     photos = Photo.objects.all()
     query = request.GET.get('q')
     if query:
         photos = Photo.objects.filter(
             Q(tag__icontains=query)
         ).distinct()
+    paginator = Paginator(photos, 6)
+    page = request.GET.get('page')
+    photos = paginator.get_page(page)
     context = {
         'photos': photos,
     }
