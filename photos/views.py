c1111cd42fcfb450a950de0b5897e43cee443cd5
david-connaughton <david.a.connaughton@gmail.com>
2020-12-18 18:28:39 +0000

    Implemented Button Filter in Photos App

--- photos/views.py
+++ photos/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Photo
 
 
 def photos(request):
     """A custom view to render Photo Page"""
     photos = Photo.objects.all()
+    query = request.GET.get('q')
+    if query:
+        photos = Photo.objects.filter(
+            Q(tag__icontains=query)
+        ).distinct()
     context = {
         'photos': photos,
     }
