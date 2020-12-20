853a9da02c4c019ecd1768545e8a62a5c2868aa5
david-connaughton <david.a.connaughton@gmail.com>
2020-12-20 18:50:55 +0000

    Added contexts.py to Cart App

---
+++ cart/contexts.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from photos.models import Photo
+
+
+def cart_contents(request):
+
+    cart_items = []
+    total = 0
+    photo_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        photo = get_object_or_404(Photo, pk=item_id)
+        total += quantity * photo.price
+        photo_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'photo': photo,
+        })
+
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'photo_count': photo_count,
+    }
+
+    return context
