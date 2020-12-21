b4366d1926b574065b47f8d5c1aa09d9e08500c5
david-connaughton <david.a.connaughton@gmail.com>
2020-12-21 03:58:41 +0000

    Added Cart Tools to Cart App

---
+++ cart/templatetags/cart_tools.py
@@ -0,0 +1,11 @@
+"""Re-purposed from Code Institute Tutorial"""
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter(name='calc_subtotal')
+def cal_subtotal(price, quantity):
+    return price * quantity
