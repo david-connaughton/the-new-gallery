89d0f1d4270567d4d16ca87b910f9fa4bb00ee25
david-connaughton <david.a.connaughton@gmail.com>
2020-12-21 12:08:16 +0000

    Created Checkout App

---
+++ checkout/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def checkout(request):
+    return render(request, 'checkout/checkout.html')
+
--- the_new_gallery/settings.py
+++ the_new_gallery/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'home',
     'photos',
     'cart',
+    'checkout',
 ]
 
 MIDDLEWARE = [
