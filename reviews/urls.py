68fe98fe3c1775cf6511ce2080f7b81c9ece698c
david-connaughton <david.a.connaughton@gmail.com>
2020-12-27 20:39:11 +0000

    Added CRUD Functionality to Reviews App

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import ReviewListView, ReviewCreateView
-from . import views
+from .views import (
+    ReviewListView, ReviewCreateView, ReviewUpdateView, ReviewDeleteView)
 
 
 urlpatterns = [
     path('', ReviewListView.as_view(), name='reviews'),
     path('review/new/', ReviewCreateView.as_view(), name='review_create'),
+    path('review/<int:pk>/update',
+         ReviewUpdateView.as_view(),
+         name='review_update'),
+    path('review/<int:pk>/delete',
+         ReviewDeleteView.as_view(),
+         name='review_delete'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.shortcuts import render, redirect
+from django.views.generic import (
+    ListView, CreateView, UpdateView, DeleteView)
 from .models import Review
 
 
@@ -16,3 +17,30 @@ class ReviewCreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class ReviewUpdateView(UpdateView):
+    model = Review
+    fields = ['favourite_image', 'review', 'rating', 'city', 'country']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def edit_function(self):
+        review = self.get_object()
+        if self.request.user == review.user:
+            return True
+        return False
+
+
+class ReviewDeleteView(DeleteView):
+    model = Review
+    success_url = '/reviews'
+
+    def edit_function(self):
+        review = self.get_object()
+        if self.request.user == review.user:
+            return True
+        return False
+
